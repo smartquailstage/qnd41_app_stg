36de5e1187a360693f16254ec4a96488b1299121
smartquailprod <smartquail.stage@gmail.com>
2023-10-17 13:59:14 -0500

    Update base.py

--- qnd41_app_stg/qnd41_app_stg/settings/base.py
+++ qnd41_app_stg/qnd41_app_stg/settings/base.py
@@ -164,7 +164,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware'
+    'django.middleware.locale.LocaleMiddleware',
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
