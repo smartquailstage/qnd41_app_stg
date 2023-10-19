3731f4b6798055fe4df725427e3cd05782636329
smartquailprod <smartquail.stage@gmail.com>
2023-10-19 11:40:10 -0500

    update

--- qnd41_app_stg/qnd41_app_stg/settings/cdn/conf.py
+++ qnd41_app_stg/qnd41_app_stg/settings/cdn/conf.py
@@ -1,16 +1,16 @@
 import os
-APP_NAME=os.environ.get("APP_NAME")
-AWS_ACCESS_KEY=os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY=os.environ.get("AWS_SECRET_ACCESS_KEY")
+APP_NAME="qnd41_app_stg"
+AWS_ACCESS_KEY="DO00YMUG6DLFPMDZFZWH"
+AWS_SECRET_ACCESS_KEY="m6qRI/NFF+moVsoSoU/CdvQOKlsAr4BIXTHWn057Tiw"
 #AWS_ACCESS_KEY= 'DO00DZXWWE3BKHHMG3NV'
 #AWS_SECRET_ACCESS_KEY= '6ZDJ/K4xQ5g/GDGb40x4ojTNfGZbn0N7xvZ4QFqJ96w'
-AWS_STORAGE_BUCKET_NAME=os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_ENDPOINT_URL=os.environ.get("AWS_S3_ENDPOINT_URL")
+AWS_STORAGE_BUCKET_NAME="qnd41-app-staticfiles"
+AWS_S3_ENDPOINT_URL="https://nyc3.digitaloceanspaces.com/"
 AWS_S3_OBJECT_PARAMETERS={
     "CacheControl": "max-age=86400", 
     "ACL": "public-read"
 }
-AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
+AWS_S3_REGION_NAME = "nyc3"
 AWS_LOCATION="https://{ AWS_STORAGE_BUCKET_NAME }.{ AWS_S3_REGION_NAME }.digitaloceanspaces.com"
 DEFAULT_FILE_STORAGE="qnd41_app_stg.settings.cdn.backends.MediaRootS3BotoStorage"
 STATICFILES_STORAGE="qnd41_app_stg.settings.cdn.backends.StaticRootS3BotoStorage"
