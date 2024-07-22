1f29dfd431b72af961eccd40b5968d7bf9d05afc
smartquailstage <smartquail.stage@gmail.com>
2024-07-22 11:20:56 -0500

    update

--- qnd41_app_stg/qnd41_app_stg/celery.py
+++ qnd41_app_stg/qnd41_app_stg/celery.py
@@ -1,14 +1,15 @@
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Establecer la configuración por defecto de Django para el programa 'celery'.
+# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41_app_stg.settings')
 
-# Crear una instancia de la aplicación Celery
 app = Celery('qnd41_app_stg')
 
-# Cargar la configuración de Celery desde el módulo de configuración de Django
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodescubrir tareas definidas en la aplicación Django
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
